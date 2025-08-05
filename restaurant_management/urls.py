c6f31c695ad9ba61361e70aa5e1294f99e4e3eee
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-08-05 21:23:24 +0530

    21135

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('api/products/',include('products.urls')),
     path('api/orders/',include('orders.urls')),
     path('menu/', views.menu, name="menu"),
+    path('',views.homepage, name="home"),
 ]
