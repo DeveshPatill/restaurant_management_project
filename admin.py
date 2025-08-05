481c8f584deb0e6d70613412a27c7585de0a48c9
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-08-05 13:58:56 +0530

    20956

---
+++ admin.py
@@ -0,0 +1,18 @@
+from django.admin import admin
+from .models import Menu, Order, OrderItem
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('name','category','price')
+    search_fields = ('name','category')
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id','customer','status','total_amount','ordered_at')
+    list_filter = ('status','ordered_at')
+    search_fields = ('customer__username')
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order','menu_item','quantity')
