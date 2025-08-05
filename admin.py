5dec0db0a5ffc4467c8729cbf2dcd04d03f4365b
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-08-05 14:09:13 +0530

    20956

--- admin.py
+++ admin.py
@@ -1,19 +1,19 @@
-from django.admin import admin
+from django.contrib import admin
 from .models import Menu, Orders, OrderItem
 
 
 @admin.register(Menu)
-class MenuAdmin(admin.AdminModel):
+class MenuAdmin(admin.ModelAdmin):
     list_display = ('name','category','price')
     search_fields = ('name','category')
 
 @admin.register(Order)
-class OrderAdmin(admin.AdminModel):
+class OrderAdmin(admin.ModelAdmin):
     list_display = ('id','customer','status','total_amount','ordered_at')
     list_filter = ('status','ordered_at')
     search_fields = ('customer__username',)
 
 @admin.register(OrderItem)
-class OrderItemAdmin(admin.AdminModel):
+class OrderItemAdmin(admin.ModelAdmin):
     list_display = ('order','menu_item','quantity')
 
