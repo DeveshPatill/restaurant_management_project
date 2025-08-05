b6248c21391b33a5c88ac244ba1de90e7f14e580
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-08-05 16:28:30 +0530

    21131

--- forms.py
+++ forms.py
@@ -5,3 +5,20 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ['name','email']
+        labels = {
+            "name": "Devesh Patil",
+            "email": "patildevesh6707@gmail.com"
+        }
+
+        def clean_name(self):
+            name = self.cleandata.get('name')
+            if len(name) < 2:
+                raise forms.ValidationError("Name Must be atleast 2 characters long.")
+            return name
+        
+        def clean_email(self):
+            email = self.cleandata.get('email')
+            if not email:
+                raise ValidationError('Email is Required.')
+            return email
+
