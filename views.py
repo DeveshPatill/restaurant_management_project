220160178287262a173b226c19900d6680213961
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-08-05 20:48:21 +0530

    21134

--- views.py
+++ views.py
@@ -2,6 +2,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 from decimal import Decimal
+from django.shortcuts import render
+from .models import MenuItem
 
 @api_view(['GET'])
 def menu_api(request):
@@ -11,3 +13,8 @@ def menu_api(request):
         {'name':'Korien Noodles', 'Description':'Spicy Noodles', 'price': 12},
     ]
     return Response({"menu":menu}, status=status.HTTP_200_OK) 
+
+# displaying menu items on a dedicated menu page
+def menu(request):
+    items = MenuItem.objects.all()
+    return render(request, "menu.html", {"items":items})
