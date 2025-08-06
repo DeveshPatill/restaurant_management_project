8bde9b54c7d0c03bfef9ab5af14ad3ba76ed8925
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-08-06 15:03:55 +0530

    22303

--- views.py
+++ views.py
@@ -36,3 +36,12 @@ def get_restaurant_name():
 def homepage_view(request):
     restaurant_name = get_restaurant_name()
     return render(request, "index.html", {"restaurant_name":restaurant_name})
+
+# for about us page for restaurant includes name, description & image
+
+def about_us_page(request):
+    return render(request, "about_us.html",{
+        "restaurant_name": get_restaurant_name(),
+        "description": "Since Opening our doors in 1999,My Spicy Restaurant has Delighted guests with bold Flavours, authentic recipies, and a vibrant dining experience. Located in the heart of the city,we specialize in spicy, flavourful dishes inspired by traditional and modern culinary traditions. Our Chefs are passionate about using High-Quality ingredients and handcafted spices to bring every dish to your life.Wheather ur here for casual lunch, a family dinner or a festival celebration, yuo'll find something unforgetable on our menu. At my Spicy Restaurant we just dont serve Food, We Serve Experiences.",
+        "image_url":"static/images/restaurant.jpg",
+    })
