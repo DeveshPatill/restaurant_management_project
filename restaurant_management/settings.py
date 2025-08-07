aadb682825bfe9068e6a225c75812c8a3197f14b
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-08-07 23:40:00 +0530

    25351

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -68,6 +68,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                #adding custom processor here
+                'core.context_processor.current_year',
             ],
         },
     },
