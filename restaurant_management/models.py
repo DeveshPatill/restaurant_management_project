5281f7c8d91ca0e144d65454257c4ed452ebfbb4
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-08-13 17:34:42 +0530

    30290

--- restaurant_management/models.py
+++ restaurant_management/models.py
@@ -24,6 +24,14 @@ class MenuItem(models.Model):
     def __str__(self):
         return self.name
 
+class ContactSubmission(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(null=True, blank=True)
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
+
 
 #python manage.py makemigrations
 #python manahe.py migrate
