611fd4c23157351bada63d2d982c6d2c7a5a8e5c
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-08-14 19:40:21 +0530

    32157

--- restaurant_management/models.py
+++ restaurant_management/models.py
@@ -19,7 +19,7 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     price  = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(uplaod_to='menu_images/', blank=True, null=True)
+    image = models.ImageField(uplaod_to='menu_images/', blank=True, null=True) #image field
 
     def __str__(self):
         return self.name
