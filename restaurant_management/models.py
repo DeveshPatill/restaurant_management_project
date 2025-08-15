b36c565e11aded2fea4d9106c024bf6db60d86cb
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-08-15 19:19:42 +0530

    32159

--- restaurant_management/models.py
+++ restaurant_management/models.py
@@ -13,7 +13,7 @@ class Restaurant(models.Model):
     address = models.TextField()
 
     def __str__(self):
-        return self.
+        return self.name
         
 class MenuItem(models.Model):
     name = models.CharField(max_length=200)
