7db42a31b79215733c719e26478b6e6c04eeb16e
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-08-20 15:46:42 +0530

    35248

---
+++ restaurant_management/context_processors.py
@@ -0,0 +1,6 @@
+from .models import Cart
+
+def cart_item_count(request):
+    if request.user.is_authentocated:
+        return {"cart_item_count":Cart.objects.filter(user=request.user).count()}
+    return {"cart_item_count": 0}
