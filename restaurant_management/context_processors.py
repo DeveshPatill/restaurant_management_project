277c8048bc9a8502d75b4c454c1e9a6ecaf78a9c
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-08-26 22:18:38 +0530

    38685

--- restaurant_management/context_processors.py
+++ restaurant_management/context_processors.py
@@ -4,3 +4,7 @@ def cart_item_count(request):
     if request.user.is_authentocated:
         return {"cart_item_count":Cart.objects.filter(user=request.user).count()}
     return {"cart_item_count": 0}
+
+
+def breadcrumb_context(request):
+    return{"breadcrumbs": getattr(request, "breadcrumbs",[])}
