28cebaa41d416ccee21ba00dbcc16039e04be525
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-08-26 13:06:42 +0530

    38687

--- restaurant_management/admin.py
+++ restaurant_management/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from .models import About
 
 admin.site.register(About)
+
+@admin.register(Special)
+class SpecialAdmin(admin.ModelAdmin):
+    list_display = ("name", "price","created_at")
