72ebba073cb213f2541e2e6bfa767b1158550952
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-08-26 13:02:11 +0530

    38687

--- restaurant_management/models.py
+++ restaurant_management/models.py
@@ -63,6 +63,15 @@ class About(models.Model):
     def __str__(self):
         return self.title
 
+class Special(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
         
 #python manage.py makemigrations
 #python manahe.py migrate
