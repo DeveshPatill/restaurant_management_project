7101c59743fd808020e83ce5c38c497aa1b2bad2
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-09-04 13:34:08 +0530

    42436

--- restaurant_management/admin.py
+++ restaurant_management/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import About
 
 admin.site.register(About)
+admin.site.register(restaurant)
 
 @admin.register(Special)
 class SpecialAdmin(admin.ModelAdmin):
