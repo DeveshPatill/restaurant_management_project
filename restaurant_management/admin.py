7d7b863cb2c4619285d7bf4e1398af437cbd0367
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-09-05 21:08:49 +0530

    42433

--- restaurant_management/admin.py
+++ restaurant_management/admin.py
@@ -3,6 +3,7 @@ from .models import About,Restaurant
 
 admin.site.register(About)
 admin.site.register(Restaurant)
+admin.site.register(Chef)
 
 @admin.register(Special)
 class SpecialAdmin(admin.ModelAdmin):
