80b613e55a9fd9e6302022befe396dff377f2b28
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-09-06 12:14:45 +0530

    42432

--- restaurant_management/models.py
+++ restaurant_management/models.py
@@ -90,5 +90,13 @@ class Chef(models.Model):
     def __str__(self):
         return self.name
 
+class MenuItem(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
 #python manage.py makemigrations
 #python manahe.py migrate
