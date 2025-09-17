73c6b445190cb8b57e030f906431e021270a1584
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-09-17 23:15:28 +0530

    44983

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+from .models import Orderstatus
+
+@receiver(post_migrate)
+def create_default_statuses(sender, **kwargs):
+    if sender.name == "orders":
+        for status in Orderstatus.DEFAULT_STATUSES:
+            Orderstatus.objects.get_or_create(name=ststus)
