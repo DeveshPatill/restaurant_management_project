9a0b548779d4c7a727b28e2d51d3f40582c1ba69
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-09-17 23:27:11 +0530

    44981

--- orders/models.py
+++ orders/models.py
@@ -39,3 +39,9 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order #{self.id} - {self.customer_name}({self.status})"
+
+class MenuCategory(models.Model):
+    name =models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
