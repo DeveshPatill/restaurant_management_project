4bb4fe9ac7e33877154d9cedf778c37820919d9f
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-09-17 23:17:12 +0530

    44983

--- orders/apps.py
+++ orders/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class OrdersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'orders'
+
+class OrdersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Orders'
+
+    def ready(self):
+        import order.signals
