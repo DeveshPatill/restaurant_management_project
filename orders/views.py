257b8b54fb5236330cbb0afef698ba90667dbb45
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-09-19 11:15:38 +0530

    48100

--- orders/views.py
+++ orders/views.py
@@ -4,6 +4,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from .models import order.MenuItem
 from .serializers import OrderSerializer,MenuItemSerializer
+from django.http import JsonResponse
+from utils.validation_utils import is_valid_email
 
 
 # Create your views here.
@@ -24,4 +26,15 @@ def menu_items_by_category(request):
         items = MenuItem.objects.all()
     serializer = MenuItemSerializer(items, many=True)
     return Response(serializer.data)
+
+def submit_email(request):
+    email = request.GET.get("email")
+    if not email:
+        return JsonResponse({"error":"Email is required"}, status=400)
+    if is_valid_email(email):
+        return JsonResponse({"message":"valid email address"})
+    else:
+        return JsonResponse({"error":"Invalid Email address"},status=400)
+
+
 
