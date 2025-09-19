ba453c6a12e318fe62c1e0ae9b1ff3592300a209
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-09-19 11:01:53 +0530

    48101

--- orders/views.py
+++ orders/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework.respones import respones
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
-from .models import order
-from .serializers import OrderSerializer
+from .models import order.MenuItem
+from .serializers import OrderSerializer,MenuItemSerializer
 
 
 # Create your views here.
@@ -14,3 +14,13 @@ class OrderHistoryView(APIView):
         orders = order.objects.filter(user=request.user)
         serializer = OrderSerializer(orders, many=True)
         return respone(serializer)
+
+@api_view(['GET'])
+def menu_items_by_category(request):
+    category = request.query_params.get('category')
+    if category:
+        items = MenuItem.objects.filter(category__name__iexact=category)
+    else:
+        items = MenuItem.objects.all()
+    serializer = MenuItemSerializer(items, many=True)
+    return Response(serializer.data)
