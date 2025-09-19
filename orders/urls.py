48597f3cd14417dbc40ed223b8163fcb019e58a9
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-09-19 11:31:56 +0530

    48099

--- orders/urls.py
+++ orders/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path("history/",OrderHistoryView.as_View(),name="order-history"),
     path('menu-items/',menu_items_by_category, name="menu-items-by-category"),
+    path("menu-items/<int:pk>/update/",UpdateMenuItemView.as_view(), name='update-menu-item')
     
 ]
