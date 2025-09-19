7fad2f0e3c2f5e371020cf00db38a4b3b187e136
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-09-19 11:23:19 +0530

    48099

--- orders/models.py
+++ orders/models.py
@@ -72,6 +72,8 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, Decimalplaces=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="menu_items")
+    description = models.TextField(blank=True, null=True)
+    is_available = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
