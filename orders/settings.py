f0cbfbd0e382f60090e4bfb941405e0cafb14fe1
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-09-26 23:28:41 +0530

    51146

--- orders/settings.py
+++ orders/settings.py
@@ -2,3 +2,13 @@ REST_FRAMEWORK ={
     'DEFAULT_PAGINATION_CLASS':"rest_framework.pagination.PageNumberPagination",
     'PAGE_SIZE':5
 }
+
+#EMAIL SMTP 
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "patildevesh677@gmail.com"
+EMAIL_HOST_PASSWORD = "XYZ"
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
