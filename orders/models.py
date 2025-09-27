ebb12d7aab6ac77cc7a3e4c1bbb9640d810ef987
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-09-27 11:52:41 +0530

    51145

--- orders/models.py
+++ orders/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
+from home.models import Product
 
 # Create your models here.
 from django.db.models.signals import post_migrate
@@ -94,3 +96,13 @@ class Restaurant(models.Model):
     
     def __str__(self):
         return self.name
+
+class Order(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    customer = models.ForeignKey(Settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='orders')
+    items = models.ManyToManyField(Product, related_name='orders')
+    total_price = models.DecimalField(max_digits=10, Decimalplaces=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"order {self.order_id} by {self.customer.username}"
