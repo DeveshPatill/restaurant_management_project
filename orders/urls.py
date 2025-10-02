4631d367cd46997c4e0313f57f431b9bd89725f1
Devesh Vijay Patil <deveshpatil450@gmail.com>
2025-10-02 19:57:42 +0530

    52873

--- orders/urls.py
+++ orders/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("menu-items/<int:pk>/update/",UpdateMenuItemView.as_view(), name='update-menu-item')
     path("menu-items/search/", MenuItemSearch.as_View(), name="menu-item-search"),
     path('api/profile/update', user_profile_update, name='profile-update',)
+    path('coupons/validate/', CouponValidationView.as_View(), name="coupon-validate"),
 ]
